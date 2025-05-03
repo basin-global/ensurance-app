68b3af23a9ab5c95b00ec908a2dbbc4a50d54bcb
tmoindustries <morgantg@gmail.com>
2025-05-02 22:22:59 -0600

    refactor: simplify TendPage by removing unused account context
    - Removed the useAccount hook and associated accountData from TendPage to streamline the component.
    - Cleaned up the code for better readability and maintainability, focusing on the core functionality of the page.

--- src/app/(accounts)/[account]/tend/page.tsx
+++ src/app/(accounts)/[account]/tend/page.tsx
@@ -1,10 +1,6 @@
 'use client'
 
-import { useAccount } from '@/modules/accounts/context'
-
 export default function TendPage() {
-  const { accountData } = useAccount()
-
   return (
     <div className="bg-gray-900/30 rounded-lg p-3">
       <h3 className="text-lg font-medium text-gray-200 mb-1">Tend Certificates</h3>
